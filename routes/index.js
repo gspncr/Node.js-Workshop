7c4ec10147b1a44d003c4f5bab978c085c7c51b3
Gary Spencer <gspenceruk+git@gmail.com>
2020-12-18 11:48:12 +0000

    workshop

---
+++ routes/index.js
@@ -0,0 +1,157 @@
+const http = require('http'),
+      path = require('path'),
+      express = require('express'),
+      newrelic = require('newrelic'),
+      router = express.Router();
+
+function nothingUseful(){
+  newrelic.startWebTransaction('aTest', () => {
+      newrelic.setTransactionName("yes a test");
+      console.log("not sure");
+      newrelic.addCustomAttribute("any","thing");
+      newrelic.endTransaction();
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  res.render(
+    'index', { 
+      title: 'New Relic Node Workshop'
+      }
+    );
+});
+
+/* GET 10
+  Generate random numbers until we get the number 10
+                                                      . */
+router.get('/badmaths', function (req, res) {
+  let x = '';
+  do {
+    x = Math.floor(Math.random() * 111000); 
+    console.log(x);
+  } while (x != 10);
+  console.log(x);
+  res.send('{"x": "'+ x + '"}\n');
+});
+
+/* GET 10
+  Run a for loop until the counter is 1 less than 11
+                                                      . */
+router.get('/maths', function (req, res) {
+  let x = '';
+  let i = 0;
+  for (i=0; i>-11;i--){
+    x = -i;
+    console.log(x);
+  }
+  console.log(x);
+  res.send('{"x": "'+ x + '"}\n');
+});
+
+/* GET 10
+  Define one as 1, add one to an array (x) 10 times, fetch length of x
+                                                      . */
+router.get('/weirdmaths', function (req, res) {
+  one = 1;
+  let x = [one, one, one, one, one, one, one, one, one, one ];
+  console.log(x.length);
+  res.send('{"x": "'+ x.length + '"}\n');
+});
+
+class NameError extends Error {
+  constructor(message, property) {
+    super(message, property);
+    this.name = "ValidationError";
+    this.property = "You got the wrong name !!!!!!";
+  }
+}
+
+router.get('/grayspencer', function (req, res) {
+  newrelic.setTransactionName(req.query.username);
+  newrelic.addCustomAttribute("ok","no");
+  newrelic.addCustomAttribute("parameterAddress", req.query.address);
+  newrelic.addCustomAttribute("parameterUsername", req.query.username);
+  throw new NameError('WrongName'); 
+});
+
+router.get('/external', function (req, res) {
+  const options = {
+      "method": "GET",
+      "hostname": "go-gary.eu-west-2.elasticbeanstalk.com",
+      "port": "80",
+      "path": "/gary",
+      "headers": {
+        "user-agent": "vscode-restclient",
+        "authorization": "token abc123",
+        "content-type": "application/json"
+      }
+    };
+    const rq = http.request(options, function (rs) {
+      const chunks = [];
+    
+      rs.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      rs.on("end", function () {
+        const body = Buffer.concat(chunks);
+        console.log(body.toString());
+        res.send(body.toString());
+      });
+    });
+    
+    rq.end();
+    
+});
+
+router.get('/external/:custom', function (req, res) {
+  const options = {
+      "method": "GET",
+      "hostname": "go-gary.eu-west-2.elasticbeanstalk.com",
+      "port": "80",
+      "path": "/" + req.params.custom,
+      "headers": {
+        "user-agent": "vscode-restclient",
+        "authorization": "token abc123",
+        "content-type": "application/json"
+      }
+    };
+    const rq = http.request(options, function (rs) {
+      const chunks = [];
+    
+      rs.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+    
+      rs.on("end", function () {
+        const body = Buffer.concat(chunks);
+        console.log(body.toString());
+        res.send(body.toString());
+      });
+    });
+    newrelic.addCustomAttribute('error', req.params.custom);
+    rq.end();
+    
+});
+
+router.get('/gary/:id', function (req, res) {
+  newrelic.addCustomAttribute('error', req.params.id);
+  const attributes = {
+      newrelic: 'implies existance of old relic',
+      hello: 'world',
+      favouriteNumber: 4
+    }
+  try {
+      newrelic.recordCustomEvent("MyFirstEvent", attributes);
+      console.log("custom event sent!");
+    } catch (e) {
+      console.log("failed to reach new relic. Status code:", e.statusCode);
+    }
+  res.send('{"yourNumber": "'+ req.params.id + '"}\n');
+  newrelic.noticeError("PROBLEM", req);
+});
+
+module.exports = router;
+setTimeout(nothingUseful, 5000);
